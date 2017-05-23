8b286bf5f13d411e3cc0192c821ffc564f23b181
simondlevy <simon.d.levy@gmail.com>
2017-05-23 18:25:40 -0400

    Added Arduino Uno pin; added license

--- BreezyCPPM.cpp
+++ BreezyCPPM.cpp
@@ -1,3 +1,23 @@
+/*
+   Implementation of BreezyCPPM class library for Arduino
+
+   Copyright (C) 2017 Greg Tomasch and Simon D. Levy
+
+   This file is part of BreezyCPPM.
+
+   BreezyCPPM is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BreezyCPPM is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with BreezyCPPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "Arduino.h"
 #include "BreezyCPPM.h"
 
